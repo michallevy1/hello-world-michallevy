79758c6189a4de78370e906291d5d18530b47afd
michallevy1 <73582267+michallevy1@users.noreply.github.com>
2020-12-24 11:53:12 +0200

    My CS includes EX7

---
+++ Michals_CV/Michal_JS.js
@@ -0,0 +1,19 @@
+fetch('https://reqres.in/api/users?page=2').then(Response=>Response.json()).then(responseJSON => createUsersList (responseJSON.data)).catch(err => console.log(err));
+    
+        function createUsersList (users){
+            console.log(users);
+            const curr_main = document.querySelector("main");
+            for(let user of users){
+                const section = document.createElement("section");
+                            section.innerHTML = `
+                    <img src="${user.avatar}" alt="Profile Picture"/>
+                    <div>
+                        <span>${user.first_name} ${user.last_name}</span>
+                        <br>
+                        <a herf="mailto:${user.email}">Send Email</a>
+                    </div>
+                    `;
+
+                curr_main.appendChild(section);
+            }
+        }
